3493c9a9fb3e6d6eab1d393a6dec98ef761c79d5
‘hang’ <2283582431@qq.com>
2023-03-10 09:18:28 +0800

    Attribute Binding

--- vue3-learning/intro-to-vue3/assets/main.js
+++ vue3-learning/intro-to-vue3/assets/main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             description: "A warm fuzzy pair of socks.",
-            image: './assets/images/socks_green.jpg',
+            image: './images/socks_green.jpg',
         }
     }
 })
